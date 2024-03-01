97c23c35c8c0447a1f9dbb54eec829e6548615ea
Astrella.Ne <mycuttingapp@gmail.com>
2024-03-01 22:56:49 +0300

    typerror fix in cli

--- gendiff/scripts/cli.py
+++ gendiff/scripts/cli.py
@@ -5,7 +5,7 @@ from gendiff.gendiff import generate_diff
 def main():
     file_path1 = input("Enter the path to the first JSON file: ")
     file_path2 = input("Enter the path to the second JSON file: ")
-    
+
     # Получаем абсолютные пути к файлам
     file1 = os.path.abspath(file_path1)
     file2 = os.path.abspath(file_path2)
