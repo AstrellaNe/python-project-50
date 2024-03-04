c57d32f4fecde5ee26ad6e9440d3b80158b50de6
Astrella.Ne <mycuttingapp@gmail.com>
2024-03-04 17:54:44 +0300

    refactored and fixed tests +: added fixtures | fixed structure of directories - gendiff.py -> scripts dir, cli.py -> gendiff dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,65 @@
 import pytest
 import os
 
-# Фикстура для пути к тестовым файлам
+# Фикстура для получения пути к директории с фикстурами
 @pytest.fixture
-def test_files_dir():
-    # Получаем абсолютный путь к директории, где находится файл conftest.py
-    return os.path.dirname(os.path.abspath(__file__))
+def fixtures_dir():
+    # Возвращаем абсолютный путь к директории с фикстурами
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures')
 
-# Фикстуры для путей к конкретным тестовым файлам
+
+# Фикстура для получения пути к файлу с исходными данными
 @pytest.fixture
-def original_file(test_files_dir):
-    return os.path.join(test_files_dir, 'original_data.json')
+def original_file(fixtures_dir):
+    # Соединяем путь к директории с именем файла
+    return os.path.join(fixtures_dir, 'original_data.json')
+
+
+# Фикстура для получения пути к измененному файлу данных
+@pytest.fixture
+def changed_file(fixtures_dir):
+    # Соединяем путь к директории с именем файла
+    return os.path.join(fixtures_dir, 'changed_data.json')
+
+
+# Фикстура для получения пути к уменьшенному файлу данных
+@pytest.fixture
+def reduced_file(fixtures_dir):
+    # Соединяем путь к директории с именем файла
+    return os.path.join(fixtures_dir, 'reduced_data.json')
+
+
+# Фикстуры для чтения ожидаемых результатов из текстовых файлов
+@pytest.fixture
+def expected_diff_original_changed(fixtures_dir):
+    # Чтение файла с ожидаемыми результатами
+    with open(os.path.join(fixtures_dir, 'expected_diff_original_changed.txt'), 'r') as file:
+        return file.read()
+
 
 @pytest.fixture
-def changed_file(test_files_dir):
-    return os.path.join(test_files_dir, 'changed_data.json')
+def expected_diff_identical_files(fixtures_dir):
+    # Чтение файла с ожидаемыми результатами
+    with open(os.path.join(fixtures_dir, 'expected_diff_identical_files.txt'), 'r') as file:
+        return file.read()
+
 
 @pytest.fixture
-def reduced_file(test_files_dir):
-    return os.path.join(test_files_dir, 'reduced_data.json')
+def expected_diff_added_data(fixtures_dir):
+    # Чтение файла с ожидаемыми результатами
+    with open(os.path.join(fixtures_dir, 'expected_diff_added_data.txt'), 'r') as file:
+        return file.read()
+
+
+@pytest.fixture
+def expected_diff_removed_data(fixtures_dir):
+    # Чтение файла с ожидаемыми результатами
+    with open(os.path.join(fixtures_dir, 'expected_diff_removed_data.txt'), 'r') as file:
+        return file.read()
+
+
+@pytest.fixture
+def expected_diff_different_types(fixtures_dir):
+    # Чтение файла с ожидаемыми результатами
+    with open(os.path.join(fixtures_dir, 'expected_diff_different_types.txt'), 'r') as file:
+        return file.read()
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,64 +1,33 @@
-# Импортируем необходимые модули
-from gendiff.gendiff import generate_diff  # Импортируем функцию
+from gendiff.scripts.gendiff import generate_diff
 
 
 def test_no_file():
     """Проверка поведения функции при отсутствии файла."""
-    # Пути к несуществующим файлам
     path_to_nonexistent_file1 = 'nonexistent_file1.json'
     path_to_nonexistent_file2 = 'nonexistent_file2.json'
-    # Проверяем, что функция возвращает None, если файлы не существуют
     assert generate_diff(path_to_nonexistent_file1, path_to_nonexistent_file2) is None
 
 
-def test_generate_diff(original_file, changed_file):
+def test_generate_diff(original_file, changed_file, expected_diff_original_changed):
     """Проверка работы функции на различных файлах."""
-    expected_output = """{
-  key: value
-- to_change: string
-+ to_change: ['list']
-- to_remove: gone
-}"""
-    assert generate_diff(original_file, changed_file) == expected_output
+    assert generate_diff(original_file, changed_file) == expected_diff_original_changed
 
 
-def test_identical_files(original_file):
-    """Тестирование с двумя идентичными файлами."""
-    expected_output = """{
-  key: value
-  to_change: string
-  to_remove: gone
-}"""
-    assert generate_diff(original_file, original_file) == expected_output
+def test_identical_files(original_file, expected_diff_identical_files):
+    """Тестирование двух идентичных файлов."""
+    assert generate_diff(original_file, original_file) == expected_diff_identical_files
 
 
-def test_added_data(original_file, changed_file):
+def test_added_data(original_file, changed_file, expected_diff_added_data):
     """Тестирование при добавлении данных во второй файл."""
-    expected_output = """{
-  key: value
-- to_change: string
-+ to_change: ['list']
-- to_remove: gone
-}"""
-    assert generate_diff(original_file, changed_file) == expected_output
+    assert generate_diff(original_file, changed_file) == expected_diff_added_data
 
 
-def test_removed_data(original_file, reduced_file):
+def test_removed_data(original_file, reduced_file, expected_diff_removed_data):
     """Тестирование при удалении данных из второго файла."""
-    expected_output = """{
-  key: value
-- to_change: string
-- to_remove: gone
-}"""
-    assert generate_diff(original_file, reduced_file) == expected_output
+    assert generate_diff(original_file, reduced_file) == expected_diff_removed_data
 
 
-def test_different_types(original_file, changed_file):
+def test_different_types(original_file, changed_file, expected_diff_different_types):
     """Тестирование при изменении типов данных между файлами."""
-    expected_output = """{
-  key: value
-- to_change: string
-+ to_change: ['list']
-- to_remove: gone
-}"""
-    assert generate_diff(original_file, changed_file) == expected_output
+    assert generate_diff(original_file, changed_file) == expected_diff_different_types
